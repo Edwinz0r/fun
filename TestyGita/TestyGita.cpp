9de53f9395c0a06076025dc1dec6f64ac4faf691
twieczorek <wieczorek.tmk@gmail.com>
2015-02-06 09:05:27 +0100

    Pola

--- TestyGita/TestyGita.cpp
+++ TestyGita/TestyGita.cpp
@@ -1,12 +1,13 @@
 // TestyGita.cpp : Defines the entry point for the console application.
 //
+//Doadłem
 
 #include "stdafx.h"
 #include <boost/test/unit_test.hpp>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::cout<<"Wow"<<std::endl;
+	std::cout<<"Wow ale fajnie"<<std::endl;
 	return 0;
 }
 
